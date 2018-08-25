0079218f97d40a5fff5dfe9980bb19226121f209
vagrant <=>
2018-08-25 02:41:26 +0000

    inicio organização dos itens

---
+++ app/Http/Requests/ItensRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ItensRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        dd();
+        return [
+            'nome'=>,'required'
+            'custo_por_unidade'=>'required',
+            'quantidade'=>'required',
+            'id_unidades_unidades'=>'required',
+            'id_tipos_item_tipos_item'=>'required'
+        ];
+    }
+}

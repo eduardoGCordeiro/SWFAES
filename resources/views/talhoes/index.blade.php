fb0ee790c079364728b43b09b658253f012a6982
eduardoGCordeiro <eduardoguilhermecordeiro@hotmail.com>
2018-08-29 20:56:29 -0300

    Criação do crud show talhoes e adição de edição e show em TalhoesController

--- resources/views/talhoes/index.blade.php
+++ resources/views/talhoes/index.blade.php
@@ -19,31 +19,31 @@
                 </div>
 
                 <div class="card-body">
-                    <div class="col-md-4">
-                        <div class="flash-message">
-                            @foreach (['danger', 'warning', 'success', 'info'] as $msg)
-                                @if(Session::has('alert-' . $msg))
+                    <div class="flash-message">
+                        @foreach (['danger', 'warning', 'success', 'info'] as $msg)
+                            @if(Session::has('alert-' . $msg))
 
-                                    <div class="alert alert-{{ $msg }} alert-dismissible">
-                                        <button type="button" class="close" data-dismiss="alert">&times;</button>
-                                        <p class="mb-0">{{ Session::get('alert-' . $msg) }}</p>
-                                    </div>
-                                @endif
-                            @endforeach
-                        </div>
+                                <div class="alert alert-{{ $msg }} alert-dismissible">
+                                    <button type="button" class="close" data-dismiss="alert">&times;</button>
+                                    <p class="mb-0">{{ Session::get('alert-' . $msg) }}</p>
+                                </div>
+                            @endif
+                        @endforeach
                     </div>
                     @foreach($talhoes as $talhao)
-                        <div class="card  border-dark col-md-3 mb-3" style="margin:10px;float:left;height: 15rem;width: 18rem !important ;">
-                            <div class="card-header">Talhão {{$talhao->id_talhoes}}<span style="float: right" class="badge badge-success">1</span></div>
-                            <div class="card-body">
-                                @if($talhao->culturas->first())
-                                    <h4 class="card-title">safra de @if($talhao->culturas->first()->tipos_safra) inverno @else verão @endif</h4>
-                                @else
-                                    <h4 class="card-title">Sem cultura atualmente</h4>
-                                @endif
-                                <p class="card-text">{{$talhao->descricao}}</p>
+                        <a href = "{{Route('talhoes.show',[$talhao->id_talhoes])}}">
+                            <div class="card  border-dark col-md-3 mb-3" style="margin:10px;float:left;height: 15rem;width: 18rem !important ;">
+                                <div class="card-header">Talhão {{$talhao->id_talhoes}}<span style="float: right" class="badge badge-success">1</span></div>
+                                <div class="card-body">
+                                    @if($talhao->culturas->first())
+                                        <h4 class="card-title">safra de @if($talhao->culturas->first()->tipos_safra) inverno @else verão @endif</h4>
+                                    @else
+                                        <h4 class="card-title">Sem cultura atualmente</h4>
+                                    @endif
+                                    <p class="card-text">{{$talhao->descricao}}</p>
+                                </div>
                             </div>
-                        </div>
+                        </a>
                     @endforeach
 
                 </div>
--- resources/views/talhoes/show.blade.php
+++ resources/views/talhoes/show.blade.php
@@ -13,24 +13,42 @@
 
                 <div class="card-header">
 
-                    <h3>Talhão #</h3>
+                    <h3>Talhão {{$talhao->id_talhoes}}</h3>
 
 
                 </div>
 
-                <div class="card-body col-md-8 offset-lg-2" >
-
-                    view show talhão
-
-                    <a href="{{Route('talhoes.edit',[$talhao->id_talhoes])}}" class="btn btn-primary">Editar</a>
-                    <form action="{{Route('talhoes.destroy',[$talhao->id_talhoes])}}" method="POST"> {{csrf_field()}}
-                        <input name="_method" type="hidden" value="DELETE">
-                        <button type="submit" class="btn btn-danger">deletar</button>
-                    </form>
-
+                <div class="card-body col-md-11" >
+                    <div class="card  border-dark col-md-4 mb-4 float-left" style="margin:10px;float:left;height: 15rem;width: 18rem !important ;">
+                        <div class="card-header">Talhão {{$talhao->id_talhoes}}<span style="float: right" class="badge badge-success">1</span></div>
+                        <div class="card-body">
+                            @if($talhao->culturas->first())
+                                <h4 class="card-title">safra de @if($talhao->culturas->first()->tipos_safra) inverno @else verão @endif</h4>
+                            @else
+                                <h4 class="card-title">Sem cultura atualmente</h4>
+                            @endif
+                            <p class="card-text">{{$talhao->descricao}}</p>
+                        </div>
+                    </div>
+                </div>
+                <div class = "col-md-10 offset-1" style="padding-bottom: 5%">
+                    <div class="panel-footer row"><!-- panel-footer -->
+                        <div class="col-xs-6 text-left">
+                            <div class="previous">
+                                <a href="{{Route('talhoes.edit',[$talhao->id_talhoes])}}" class="btn btn-primary">Editar</a>
+                            </div>
+                        </div>
+                        <div class="col-xs-6 text-right">
+                            <div class="next offset-1">
+                                <form action="{{Route('talhoes.destroy',[$talhao->id_talhoes])}}" method="POST"> {{csrf_field()}}
+                                    <input name="_method" type="hidden" value="DELETE">
+                                    <button type="submit" class="btn btn-danger">deletar</button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection

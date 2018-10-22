64edcafe379e67e2d83540f2f0ed005b1ac085e2
Luis Thiago <mailluisthiago@gmail.com>
2018-10-22 10:21:24 +0000

    alteração config name commits

---
+++ script.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+git filter-branch --env-filter '
+OLD_NAME="vagrant"
+CORRECT_NAME="Luis Thiago"
+CORRECT_EMAIL="mailluisthiago@gmail.com"
+if [ "$GIT_COMMITTER_NAME" = "$OLD_NAME" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_NAME" = "$OLD_NAME" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags

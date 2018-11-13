c80109f1d2b7aec6bf2e2ae1d98537cb72f114d3
Luis Thiago Padilha <mailluisthiago@gmail.com>
2018-11-13 11:05:09 +0000

    middleware auth nas rotas

--- routes/web.php
+++ routes/web.php
@@ -30,7 +30,7 @@ Route::get('/inicio', 'HomeController@index')->name('home');
 // data tables
 //Route::get('adms_talhoes/{id}/getdata', 'AdmsTalhoesController@data_tables')->name('data_table_adms_talhoes');
 //ressource
-Route::resource('adms_talhoes', 'AdmsTalhoesController');
+Route::resource('adms_talhoes', 'AdmsTalhoesController')->middleware('auth');
 //------------------------------------------------------------------------------//
 
 
@@ -42,7 +42,7 @@ Route::get('atividades/{atividade}/atividades_list_transactions', 'AtividadesCon
 
 //ressource
 
-Route::resource('atividades', 'AtividadesController');
+Route::resource('atividades', 'AtividadesController')->middleware('auth');
 //------------------------------------------------------------------------------//
 
 
@@ -56,7 +56,7 @@ Route::get('requisicoes/getdata', 'RequisicoesController@data_tables')->name('da
 //ressource
 Route::get('requisicoes/{id}/moderar', 'RequisicoesController@moderar_get')->name('moderar_get');
 Route::post('requisicoes/{id}/moderar', 'RequisicoesController@moderar')->name('moderar_post');
-Route::resource('requisicoes', 'RequisicoesController');
+Route::resource('requisicoes', 'RequisicoesController')->middleware('auth');
 //------------------------------------------------------------------------------//
 
 
@@ -70,34 +70,34 @@ Route::resource('requisicoes', 'RequisicoesController');
 // data tables
 Route::get('itens/getdata', 'ItensController@data_tables')->name('data_table_itens');
 //ressource
-Route::resource('itens', 'ItensController');
+Route::resource('itens', 'ItensController')->middleware('auth');
 //------------------------------------------------------------------------------//----------------- rotas referentes à FuncionariosController --------------------- //
 // data tables
 Route::get('funcionarios/getdata', 'FuncionariosController@data_tables')->name('data_table_funcionarios');
 Route::get('funcionarios/{id}/getdata_talhoes', 'FuncionariosController@data_tables_talhoes')->name('data_table_funcionarios_talhoes');
 //ressource
-Route::resource('funcionarios', 'FuncionariosController');
+Route::resource('funcionarios', 'FuncionariosController')->middleware('auth');
 //------------------------------------------------------------------------------//
 
 //-------------------- rotas referentes à ItensController --------------------- //
 // data tables
 Route::get('unidades/getdata', 'UnidadesController@data_tables')->name('data_table_unidades');
 //ressource
-Route::resource('unidades', 'UnidadesController');
+Route::resource('unidades', 'UnidadesController')->middleware('auth');
 //------------------------------------------------------------------------------//
 
 //-------------------- rotas referentes à TipoItemController ------------------ //
 // data tables
 Route::get('tipo_item/getdata', 'TipoItemController@data_tables')->name('data_table_tipo_item');
 //ressource
-Route::resource('tipo_item', 'TipoItemController');
+Route::resource('tipo_item', 'TipoItemController')->middleware('auth');
 //------------------------------------------------------------------------------////-------------------- rotas referentes à CulturasController ------------------ //
 // data tables
 Route::get('culturas/getdata', 'CulturasController@data_tables')->name('data_table_culturas');
 //finalizar
 Route::post('culturas/{id}/finalizar', 'CulturasController@finalizar')->name('finalizar_culturas');
 //ressource
-Route::resource('culturas', 'CulturasController');
+Route::resource('culturas', 'CulturasController')->middleware('auth');
 //------------------------------------------------------------------------------//
 
 
@@ -108,13 +108,13 @@ Route::get('movimentacoes/getdata', 'MovimentacoesController@data_tables')->name
 //ressource
 Route::get('movimentacoes/{atividade}/create', 'MovimentacoesController@create_by_activity')->name('transaction_by_activity');
 Route::post('movimentacoes/{atividade}/create', 'MovimentacoesController@create_by_activity_post')->name('transaction_by_activity_post');
-Route::resource('movimentacoes', 'MovimentacoesController');
+Route::resource('movimentacoes', 'MovimentacoesController')->middleware('auth');
 //------------------------------------------------------------------------------//
 
 
 //-------------------- rotas referentes à TalhoesController --------------------- //
 //ressource
-Route::resource('talhoes', 'TalhoesController');
+Route::resource('talhoes', 'TalhoesController')->middleware('auth');
 // data tables
 Route::get('talhoes/getdata', 'TalhoesController@data_tables')->name('data_table_talhoes');
 //------------------------------------------------------------------------------//
@@ -129,7 +129,7 @@ Route::get('talhoes/getdata', 'TalhoesController@data_tables')->name('data_table
 // data tables
 Route::get('tipos_atividades/getdata', 'TiposAtividadesController@data_tables')->name('data_table_tipos_atividades');
 //ressource
-Route::resource('tipos_atividades', 'TiposAtividadesController');
+Route::resource('tipos_atividades', 'TiposAtividadesController')->middleware('auth');
 //------------------------------------------------------------------------------//
 
 
@@ -137,4 +137,4 @@ Route::resource('tipos_atividades', 'TiposAtividadesController');
 Route::get('relatorios/estoque', 'RelatoriosController@estoque')->name('estoque-rel');
 Route::get('relatorios/atividades', 'RelatoriosController@atividades')->name('atividades-rel');
 Route::get('relatorios/movimentacoes', 'RelatoriosController@movimentacoes')->name('movimentacoes-rel');
-Route::resource('relatorios', 'RelatoriosController');
+Route::resource('relatorios', 'RelatoriosController')->middleware('auth');

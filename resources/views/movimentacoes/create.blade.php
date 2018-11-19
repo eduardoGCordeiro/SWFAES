a62548295686c6bd5eee6f8d5a96239fc48a8e89
Luis Thiago Padilha <mailluisthiago@gmail.com>
2018-11-19 01:32:18 +0000

    correção cadastro de movimentação para a segunda atividade.

--- resources/views/movimentacoes/create.blade.php
+++ resources/views/movimentacoes/create.blade.php
@@ -58,10 +58,10 @@
                         @else
                             <div class="form-group row">
                                 <label class="col-lg-4 col-form-label text-lg-right">Atividade</label>
-                                <div class="col-lg-2">
+                                <div class="col-lg-5">
 
 
-                                    <input name="id_atividades_atividades" class="form-control" value="{{$atividade}}" id="readOnlyInput" type="text" placeholder="Readonly input here…" readonly="">
+                                    <input name="id_atividades_atividades" class="form-control" value="{{\App\Atividade::find($atividade)->descricao}}" id="readOnlyInput" type="text" placeholder="Readonly input here…" readonly="">
 
 
                                     @if ($errors->has('id_atividades_atividades'))
